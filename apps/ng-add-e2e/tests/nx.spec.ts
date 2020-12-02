28ed70669a914dc8cec1488beecf6865f85bc8cc
Chau Tran <nartc7789@gmail.com>
2020-12-01 20:55:51 -0600

    wip

--- apps/ng-add-e2e/tests/nx.spec.ts
+++ apps/ng-add-e2e/tests/nx.spec.ts
@@ -1,5 +1,5 @@
 import {
-  ensureNxProject,
+  cleanup,
   runCommandAsync,
   runNxCommandAsync,
 } from '@nrwl/nx-plugin/testing';
@@ -7,11 +7,16 @@ import {
   detectPackageManager,
   getPackageManagerInstallCommand,
 } from '@nrwl/workspace/src/utils/detect-package-manager';
+import { ensureNgProject, ensureNxProject } from '../utils';
 
 jest.setTimeout(999999);
 
 describe('nx e2e', () => {
-  it('should work', async (done) => {
+  // afterEach(() => {
+  //   cleanup();
+  // });
+
+  it('should work with nx', async (done) => {
     ensureNxProject('@ngneat/tailwind', 'dist/libs/ng-add');
 
     await runCommandAsync(
@@ -25,6 +30,32 @@ describe('nx e2e', () => {
       'generate @nrwl/angular:application nx-test --e2eTestRunner=none --skipTests --unitTestRunner=none --inlineStyle --inlineTemplate --directory=sub --linter=eslint --style=scss --routing=false'
     );
 
+    await runNxCommandAsync(`generate @ngneat/tailwind:ng-add`);
+
+    done();
+  });
+
+  it('should work with nx + angular cli', async (done) => {
+    ensureNxProject('@ngneat/tailwind', 'dist/libs/ng-add', 'ng');
+
+    await runCommandAsync(
+      `${getPackageManagerInstallCommand(
+        detectPackageManager(),
+        true
+      )} @nrwl/angular`
+    );
+
+    await runNxCommandAsync(
+      'generate @nrwl/angular:application nx-test --e2eTestRunner=none --skipTests --unitTestRunner=none --inlineStyle --inlineTemplate --directory=sub --linter=eslint --style=scss --routing=false'
+    );
+
+    await runNxCommandAsync(`generate @ngneat/tailwind:ng-add`);
+
+    done();
+  });
+
+  fit('should work with angular cli', async (done) => {
+    ensureNgProject('@ngneat/tailwind', 'dist/libs/ng-add');
     await runNxCommandAsync(`generate @ngneat/tailwind:ng-add`);
 
     done();
