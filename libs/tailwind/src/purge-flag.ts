f98daccb44c8f3b726f1f64fa3487747b65c40db
Vlad <vl.tansky@gmail.com>
2021-03-11 09:50:19 +0200

    test: guessProductionMode

--- libs/tailwind/src/purge-flag.ts
+++ libs/tailwind/src/purge-flag.ts
@@ -1,7 +1,8 @@
 export function guessProductionMode() {
   const argv = process.argv.join(' ').toLowerCase();
-  const isBuild = argv.includes("build");
+  const isBuild = argv.includes(" build");
+  const isBuildAlias = argv.includes("ng b");
   const isFlag = argv.includes("--prod");
   const isProdEnv = process.env.NODE_ENV === 'production';
-  return isBuild || isFlag || isProdEnv;
+  return isBuild || isFlag || isProdEnv || isBuildAlias;
 }
